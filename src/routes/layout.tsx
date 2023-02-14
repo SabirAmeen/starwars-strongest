bf51d6866f74a217ced9e73d91f7c0b96c0d46e1
Sabir <sabir@qburst.com>
2023-02-14 13:13:08 +0530

    initial UI changes

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,16 +1,8 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { loader$ } from '@builder.io/qwik-city';
 
 import Header from '../components/header/header';
 
-export const serverTimeLoader = loader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
-
 export default component$(() => {
-  const serverTime = serverTimeLoader.use();
   return (
     <>
       <main>
@@ -19,11 +11,8 @@ export default component$(() => {
           <Slot />
         </section>
       </main>
-      <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
-          <div>{serverTime.value.date}</div>
-        </a>
+      <footer class='main-footer'>
+        Made with ♡ by Sabir Ameen
       </footer>
     </>
   );
